043263e2c078914a7c70d8dec7e94f04f6611abc
Felipe Matrone <felipematrone@gmail.com>
2026-03-01 18:01:12 -0300

    Class StatusHistorico Criado

---
+++ src/HistoricoStatus.java
@@ -0,0 +1,35 @@
+import java.time.LocalDateTime;
+
+public class HistoricoStatus {
+
+    private Status status;
+    private LocalDateTime data;
+    private String descricao;
+
+public HistoricoStatus( Status status, String descricao) {
+    this.status = status;
+    this.data = LocalDateTime.now();
+    this.descricao = descricao;
+  }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+}
---
+++ src/Status.java
@@ -0,0 +1,10 @@
+public enum Status {
+    ANALISE,
+    TRIAGEM,
+    EM_PROCESSO,
+    EM_EXECUCAO,
+    RESOLVIDO,
+    ENCERRADO
+}
+
+//Enum = Conjunto fixo de valores
