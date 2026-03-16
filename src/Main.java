9689e0a5ccc4b6c9837a38238d2b39dd12d6e93f
Felipe Matrone <felipematrone@gmail.com>
2026-03-16 20:32:30 -0300

    Class Solicitacoes criadas e Testado no main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Usuario usuario01 = new Usuario("Felipe M.", "felipematrone@gmail.com", "11553125353", "44991024053", false);
-
+        Usuario usuario01 = new Usuario("Felipe M.", "felipematrone@gmail.com", "44991024053", "11553125353", false );
 
         System.out.println("------ TESTE USUARIO REGISTRADO ------");
         System.out.println("Nome: " + usuario01.getNome());
@@ -10,6 +9,17 @@ public class Main {
         System.out.println("Telefone: " + usuario01.getTelefone());
         System.out.println("CPF: " + usuario01.getCpf());
         System.out.println("Registrado: " + usuario01.getRegistrado());
+
+        Solicitacoes solicitacao = new Solicitacoes( usuario01, "PR", "Maringá", "Buraco grande na rua", "Infraestrutura", true );
+
+        System.out.println("\n------ TESTE SOLICITAÇÃO ------");
+        System.out.println("Protocolo: " + solicitacao.getProtocolo());
+        System.out.println("Estado: " + solicitacao.getEstado());
+        System.out.println("Cidade: " + solicitacao.getCidade());
+        System.out.println("Status atual: " + solicitacao.getStatusAtual());
+        System.out.println("Data abertura: " + solicitacao.getDataAbertura());
+        System.out.println("Risco de vida: " + solicitacao.getRiscoVida());
+
 
     }
 }
---
+++ src/Solicitacoes.java
@@ -0,0 +1,85 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solicitacoes {
+
+    private int contador = 1;
+    private String protocolo;
+    private Usuario usuario;
+    private String estado;
+    private String cidade;
+    private String descricao;
+    private String categoria;
+    private boolean riscoVida;
+
+    private Status statusAtual;
+    private LocalDateTime dataAbertura;
+
+    private List<HistoricoStatus> historico;
+
+    public Solicitacoes( Usuario usuario, String estado, String cidade, String descricao, String categoria, boolean riscoVida ) {
+
+        this.protocolo = String.format( "%06d", contador++ );
+        this.usuario   = usuario;
+        this.estado    = estado;
+        this.cidade    = cidade;
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.riscoVida = riscoVida;
+        this.statusAtual = Status.ANALISE;
+        this.dataAbertura = LocalDateTime.now();
+        this.historico = new ArrayList<>();
+
+        historico.add( new HistoricoStatus( Status.ANALISE, "Solicitação criada com sucesso!" ) );
+    }
+
+    public String getProtocolo() {
+        return protocolo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public boolean getRiscoVida() {
+        return riscoVida;
+    }
+
+    public void setRiscoVida( boolean riscoVida ) {
+        this.riscoVida = riscoVida;
+    }
+
+    public Status getStatusAtual() {
+        return statusAtual;
+    }
+
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public List<HistoricoStatus> getHistorico() {
+        return historico;
+    }
+
+    public void atualizarStatus( Status novoStatus, String comentario ) {
+        this.statusAtual = novoStatus;
+        historico.add(new HistoricoStatus(novoStatus, comentario ) );
+    }
+}
