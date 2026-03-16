05e3e4a98d0136b7a0e0e4b62c762520ca664d05
Felipe Matrone <felipematrone@gmail.com>
2026-03-16 19:49:38 -0300

    Teste NotBook

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-void main() {
+public class Main {
+    public static void main(String[] args) {
 
-    Usuario usuario01 = new Usuario( "Felipe M.", "felipematrone@gmail.com", "11553125353", "44991024053", false);
-
-
-    System.out.println("------ TESTE USUARIO REGISTRADO ------");
-    System.out.println("Nome: " + usuario01.getNome() );
-    System.out.println("Email: " + usuario01.getEmail() );
-    System.out.println("Telefone: " + usuario01.getTelefone() );
-    System.out.println("CPF: " + usuario01.getCpf() );
-    System.out.println("Registrado: " + usuario01.getRegistrado() );
+        Usuario usuario01 = new Usuario("Felipe M.", "felipematrone@gmail.com", "11553125353", "44991024053", false);
 
 
+        System.out.println("------ TESTE USUARIO REGISTRADO ------");
+        System.out.println("Nome: " + usuario01.getNome());
+        System.out.println("Email: " + usuario01.getEmail());
+        System.out.println("Telefone: " + usuario01.getTelefone());
+        System.out.println("CPF: " + usuario01.getCpf());
+        System.out.println("Registrado: " + usuario01.getRegistrado());
 
-
+    }
 }
