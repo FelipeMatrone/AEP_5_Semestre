9b0c8a9b42c75915af1220f1179a976966e2d45f
Felipe Matrone <felipematrone@gmail.com>
2026-03-20 19:29:13 -0300

    ServicosUsuario criado

---
+++ src/ServicosUsuario.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServicosUsuario {
+
+    private List<Usuario> usuarios = new ArrayList<>();
+    public void registrar( Usuario usuario ) {
+
+        for ( Usuario usu : usuarios ) {
+            if ( usu.getCpf().equals( usuario.getCpf() ) ) {
+                System.out.println( "Usuário já cadastrado!" );
+                return;
+            }
+        }
+
+        usuarios.add( usuario );
+        System.out.println( "Usuário salvo na memória." );
+    }
+
+    public Usuario login( String cpf ) {
+
+        for ( Usuario usuario : usuarios ) {
+            if ( usuario.getCpf().equals( cpf.trim() ) ) {
+                return usuario;
+            }
+        }
+        return null;
+    }
+}
--- src/Usuario.java
+++ src/Usuario.java
@@ -6,52 +6,36 @@ public class Usuario {
     private String telefone;
     private boolean registrado;
 
-    public Usuario( String nome, String email, String cpf, String telefone, boolean registrado ){
+    public Usuario(String nome, String email, String telefone, String cpf){
         this.nome = nome;
         this.email = email;
-        this.cpf = cpf;
         this.telefone = telefone;
-        this.registrado = registrado;
+        this.cpf = cpf;
+        this.registrado = true;
+    }
+
+    public Usuario(String cpf) {
+        this.cpf = cpf;
+        this.registrado = false;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public boolean getRegistrado() {
         return registrado;
-    }
-
-    public void setRegistrado(boolean registrado) {
-        this.registrado = registrado;
     }
 
     public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getTelefone(){
         return telefone;
     }
-
-    public void setTelefone(String telefone){
-        this.telefone = telefone;
-    }
-
 }
